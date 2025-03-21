8b1e16de274af96a7a1803e5fb49028e141aa07e
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-03-22 01:16:12 +0530

    loopchallanges

---
+++ JSFOUNDATION/part4/loopchallange.js
@@ -0,0 +1,17 @@
+let total = 0;
+let k = 1;
+do {
+    total += k;
+    k++;
+} while (k <= 3);
+// console.log(total);
+
+ let multipliedNumbers = []
+ let numbers = [2,4,6]
+
+ for (let l = 0; l < numbers.length; l++) {
+takeNumber = numbers[l] * 2;
+multipliedNumbers.push(takeNumber);
+}
+// console.log(multipliedNumbers);
+
