ad0930e2f6e1c7e38055a3b479b4a8fc88990a47
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-03-23 21:29:25 +0530

    only10minstudy

---
+++ JSFOUNDATION/part4/levelupchallange.js
@@ -0,0 +1,12 @@
+let teas = ["green tea","black tea","chai","oolong tea"];
+let selectedTeas = [];
+
+for (let i = 0; i < teas.length; i++) {
+    
+    if(teas[i] == "chai"){
+        break;
+    }
+    selectedTeas.push(teas[i]);
+}
+// console.log(selectedTeas);
+
