74f5e679a065e816652abd048cb1540b22ac3843
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-04-03 16:50:33 +0530

    dom

--- JSFOUNDATION/part8/script.js
+++ JSFOUNDATION/part8/script.js
@@ -40,3 +40,42 @@ document.getElementById("removeLastTask").addEventListener('click',function(){
   let taskList = document.getElementById("taskList");
   taskList.lastElementChild.remove();
 })
+
+// example 6
+
+document.getElementById("clickMeButton").addEventListener('mouseover',function(){
+  alert("chaicode")
+})
+
+// example 7
+
+document.getElementById("teaList").addEventListener('click',function(event){
+if(event.target && event.target.matches(".teaItem")){
+  alert("You selected: "+ event.target.textContent)
+}
+
+})
+
+// example 8
+
+document.getElementById("feedbackform").addEventListener("submit", function (event){
+  event.preventDefault();
+  let feedback = document.getElementById("feedbackInput").value;
+  console.log(feedback);
+  document.getElementById("feedbackDisplay").textContent = `Feedback is: ${feedback}`
+  
+});
+
+// example 9
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("domStatus").textContent = "DOM fully loaded"
+}
+);
+
+// example 10
+
+document.getElementById("toggleHighlight").addEventListener("click",function(){
+  let descriptionText = document.getElementById("descriptionText");
+  descriptionText.classList.toggle("highlight");
+})
