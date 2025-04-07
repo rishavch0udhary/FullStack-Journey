0101262cae477a9d16d8dd59d7fc769153e5b563
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-04-07 16:44:07 +0530

    function

---
+++ JS journey/13-function.js
@@ -0,0 +1,35 @@
+// function myfun(){
+//     console.log("hello");
+//     console.log("Deathcode")
+// }
+// myfun()
+
+// function add5and6(){
+//     let sum = 5+6
+//     return sum
+// }
+// // let sum = add5and6()
+// console.log(add5and6());
+
+// function addTwoNumbers(a,b){
+//     let sum = a+b;
+//     console.log(sum);    
+// }
+// addTwoNumbers(6,9)
+
+// function addTwoNumbers(a,b){
+//     let total = a+b;
+//     return total
+// }
+// console.log(addTwoNumbers(14,14));
+
+// function addNumbers(...numbers){
+//     let sum = 0;
+//     for(let i=0; i<numbers.length;i++){
+// sum += numbers[i];
+//     }
+//     return sum
+// }
+// console.log(addNumbers(22,33,37,8));
+
+
