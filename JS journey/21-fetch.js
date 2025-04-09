d8389fba6df39e2bd071d3ab0d8c8d3122abe706
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-04-10 00:09:01 +0530

    fetch

---
+++ JS journey/21-fetch.js
@@ -0,0 +1,14 @@
+// fetch("https://dummyjson.com/products").then((res)=>{
+//     return res.json()
+// }).catch((error)=>{
+//     console.log(error);
+// }).then((data)=>{
+//     console.log(data)
+// })
+
+// async function fetchData() {
+//     let res = await fetch("https://dummyjson.com/products")
+//     let data = await res.json()
+//     console.log(data);
+// }
+// fetchData()
